c4ad10eef8265a545a9cba608b94ef53fd76a568
pcarpiano <pcarpiano@gmail.com>
2026-09-14 00:20:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function openGift() {
-    alert("You found your birthday gift! 🎁");
+    document.getElementById("opening").classList.add("hidden");
+
+    document.getElementById("birthday").classList.remove("hidden");
+}
+
+function showMessage() {
+    document.getElementById("birthday").classList.add("hidden");
+
+    document.getElementById("message").classList.remove("hidden");
 }
